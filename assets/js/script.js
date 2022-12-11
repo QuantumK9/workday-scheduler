39f6429e5bbdcad42bb28a3d5eaa9f7ba9dfab0d
QuantumK <quantumk9@hotmail.com>
2022-12-11 17:13:55 +0000

    add function that compares time and changes color value for each slot

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // add day at top with moment.js
 
 // from moment with current time iterate through rows and change color accordingly
-
+var currentTime = 10; // temporary "time" for testing
 // take text input from textareas (along with id ? )
 
 var events = [
@@ -69,11 +69,21 @@ function addInputToEvents(inputVal, eventId) {
   events[eventId - 1].event = inputVal;
 }
 
+//function that will compare time slot with current time and change color of fields
+function compareTime(currentTime, slotTime, eventIndex) {
+  if (slotTime < currentTime) {
+    events[eventIndex].color = "grey";
+  } else if (slotTime === currentTime) {
+    events[eventIndex].color = "red";
+  } else {
+    events[eventIndex].color = "red";
+  }
+}
 // add localstorage to save areas
 
 $("document").ready(function () {
   // console.log("document is ready and jQuery is running!");
-
+  compareTime(currentTime, 9, 0);
   $("#form-1").on("submit", function (ev) {
     ev.preventDefault();
     var textOneEl = $("#text-area-1").val();

a6120d967a2546fe2f461fc43bc0e27b17ff87bc
QuantumK <quantumk9@hotmail.com>
2022-12-13 17:35:26 +0000

    complete functions that take inputs and stores them to events array and color the spaces by comparing with current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
 // console.log("connected!");
 
+// select all forms
+var form1El = $("#form-1");
+var form2El = $("#form-2");
+var form3El = $("#form-3");
+var form4El = $("#form-4");
+var form5El = $("#form-5");
+var form6El = $("#form-6");
+var form7El = $("#form-7");
+var form8El = $("#form-8");
+var form9El = $("#form-9");
 // add day at top with moment.js
 
 // from moment with current time iterate through rows and change color accordingly
-var currentTime = 10; // temporary "time" for testing
+var currentTime = 12; // temporary "time" for testing
 // take text input from textareas (along with id ? )
 
 var events = [
@@ -11,84 +21,122 @@ var events = [
     id: 1,
     time: "9am",
     event: "",
-    color: "green",
   },
 
   {
     id: 2,
     time: "10am",
     event: "",
-    color: "green",
   },
   {
     id: 3,
     time: "11am",
     event: "",
-    color: "green",
   },
   {
     id: 4,
     time: "12pm",
     event: "",
-    color: "green",
   },
   {
     id: 5,
     time: "1pm",
     event: "",
-    color: "green",
   },
   {
     id: 6,
     time: "2pm",
     event: "",
-    color: "green",
   },
   {
     id: 7,
     time: "3pm",
     event: "",
-    color: "green",
   },
   {
     id: 8,
     time: "4pm",
     event: "",
-    color: "green",
   },
   {
     id: 9,
     time: "5pm",
     event: "",
-    color: "green",
   },
 ];
+
+//function that will display and change color of each slot based on time comparison with current into grey, red or green
+function addColorClassToElement(element, currentTime) {
+  let slotTimeNumber = parseInt(element.attributes["data-time"].value);
+  if (slotTimeNumber < currentTime) {
+    element.classList.add("past");
+  } else if (slotTimeNumber === currentTime) {
+    element.classList.add("present");
+  } else {
+    element.classList.add("future");
+  }
+}
+
+// function that takes input from form and adds it to events array
+
+function addingFormInputToEvents(formId, eventsId) {
+  textEl = $(formId).val().trim();
+  console.log(textEl);
+  addInputToEvents(textEl, eventsId);
+  console.log(events[eventsId - 1]);
+}
 
 // function that adds input to events array
 function addInputToEvents(inputVal, eventId) {
   events[eventId - 1].event = inputVal;
 }
-
-//function that will compare time slot with current time and change color of fields
-function compareTime(currentTime, slotTime, eventIndex) {
-  if (slotTime < currentTime) {
-    events[eventIndex].color = "grey";
-  } else if (slotTime === currentTime) {
-    events[eventIndex].color = "red";
-  } else {
-    events[eventIndex].color = "red";
-  }
-}
 // add localstorage to save areas
 
 $("document").ready(function () {
   // console.log("document is ready and jQuery is running!");
-  compareTime(currentTime, 9, 0);
-  $("#form-1").on("submit", function (ev) {
+  // let inputTdElements = $(".input-table-data");
+  let inputTdElements = document.querySelectorAll(".input-table-data");
+
+  // console.log(inputTdElements);
+
+  inputTdElements.forEach(function (element) {
+    addColorClassToElement(element, currentTime);
+  });
+
+  form1El.on("submit", function (ev) {
     ev.preventDefault();
-    var textOneEl = $("#text-area-1").val();
-    console.log(textOneEl);
-    addInputToEvents(textOneEl, 1);
-    console.log(events[0].event);
+    addingFormInputToEvents("#text-area-1", 1);
+  });
+  form2El.on("submit", function (ev) {
+    ev.preventDefault();
+    addingFormInputToEvents("#text-area-2", 2);
+  });
+  form3El.on("submit", function (ev) {
+    ev.preventDefault();
+    addingFormInputToEvents("#text-area-3", 3);
+  });
+  form4El.on("submit", function (ev) {
+    ev.preventDefault();
+    addingFormInputToEvents("#text-area-4", 4);
+  });
+  form5El.on("submit", function (ev) {
+    ev.preventDefault();
+    addingFormInputToEvents("#text-area-5", 5);
+  });
+  form6El.on("submit", function (ev) {
+    ev.preventDefault();
+    addingFormInputToEvents("#text-area-6", 6);
+  });
+  form7El.on("submit", function (ev) {
+    ev.preventDefault();
+    addingFormInputToEvents("#text-area-7", 7);
+  });
+  form8El.on("submit", function (ev) {
+    ev.preventDefault();
+    addingFormInputToEvents("#text-area-8", 8);
+  });
+  form9El.on("submit", function (ev) {
+    ev.preventDefault();
+    addingFormInputToEvents("#text-area-9", 9);
   });
 });

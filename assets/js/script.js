20003a791b25658f734f80058678d8e702ec8079
QuantumK <quantumk9@hotmail.com>
2022-12-30 00:13:11 +0000

    refactor functions and clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,6 +51,7 @@ var events = [
 // getting date and time in the right formats from moment.js
 var currentDateDisplay = moment().format("dddd Do MMMM YYYY");
 var currentTime = moment().format("H");
+// var currentTime = 12; // for debugging
 
 // on page load
 $("document").ready(function () {
@@ -65,52 +66,50 @@ $("document").ready(function () {
 
   // get an array of al input elements
   let inputTdElements = document.querySelectorAll(".input-table-data");
-
-  inputTdElements.forEach(function (element) {
-    addColorClassToElement(element, currentTime);
-  });
+  // style input slots according to current time
+  addColorClassToElements(inputTdElements, currentTime);
 });
 
 // event listener on all save buttons
 $(document).on("click", ".saveBtn", function (ev) {
   ev.preventDefault();
   var formInput = $(this)[0].form.childNodes[1].value.trim();
-
   var inputId = parseInt($(this).attr("data-id"));
-  var inputTime = parseInt($(this).attr("data-time"));
   addInputToEvents(formInput, inputId);
   addEventsToLocalStorage(events);
   renderEvents(events);
 });
 
 // helper functions
-function renderEvents(eventsArray) {
-  eventsArray.map((event) => {
-    var strEventId = event.id.toString();
-    $(`#${strEventId}`).val("");
-    $(`#${strEventId}`).val(event.event);
-    // console.log(event.id);
-    console.log(strEventId);
-    console.log(event.event);
-  });
-}
 
+// display date
 function renderDate(date) {
   $("#currentDay").text(date);
 }
+
 //function that will display and change color of each slot based on time comparison with current into grey, red or green
-function addColorClassToElement(element, currentTime) {
-  let slotTimeNumber = parseInt(element.attributes["data-time"].value);
-  if (slotTimeNumber < currentTime) {
-    element.classList.add("past");
-  } else if (slotTimeNumber === currentTime) {
-    element.classList.add("present");
-  } else {
-    element.classList.add("future");
-  }
+function addColorClassToElements(elementsArray, currentTime) {
+  elementsArray.forEach(function (element) {
+    var slotTimeNumber = parseInt(element.attributes["data-time"].value);
+    if (slotTimeNumber < currentTime) {
+      element.classList.add("past");
+    } else if (slotTimeNumber === currentTime) {
+      element.classList.add("present");
+    } else {
+      element.classList.add("future");
+    }
+  });
 }
 
-//
+// render events
+function renderEvents(eventsArray) {
+  eventsArray.map(function (event) {
+    var strEventId = event.id.toString();
+    $(`#${strEventId}`).val("");
+    $(`#${strEventId}`).val(event.event);
+  });
+}
+
 // function that adds input to events array
 function addInputToEvents(inputVal, eventId) {
   events[eventId].event = inputVal;
